ea0ad9912d2b000c18e2e63e974a4f6d65065a9b
Vera <veraqwer1234@gmail.com>
2025-09-12 22:53:43 +0300

    NIST tests

---
+++ lab_2/tests.py
@@ -0,0 +1,73 @@
+import math
+from scipy.special import gammaincc
+
+
+def frequency_bit_test(sequence: str) -> float:
+    """
+    Выполняет частотный побитовый тест NIST.
+    :param sequence: бинарная последовательность
+    :return: P-значение теста
+    """
+    s_n = (sequence.count('1') - sequence.count('0')) / math.sqrt(len(sequence))
+    p_value = math.erfc(s_n / math.sqrt(2))
+
+    return p_value
+
+
+def identical_consecutive_bits_test(sequence: str) -> float:
+    """
+    Выполняет тест на одинаковые подряд идущие биты.
+    :param sequence: бинарная последовательность
+    :return: P-значение теста
+    """
+    n = len(sequence)
+    zeta = sequence.count('1') / n
+
+    if abs(zeta - 0.5) >= 2 /math.sqrt(n):
+        return 0.0
+
+    v_n = sum(1 if sequence[i] != sequence[i + 1] else 0 for i in range(n - 1))
+    p_value = math.erfc(abs(v_n - 2 * n * zeta * (1 - zeta)) / (2 * math.sqrt(2 * n) * zeta * (1 - zeta)))
+
+    return p_value
+
+
+def longest_sequence_in_block_test(sequence: str, pi: list[float]) -> float:
+    """
+    Выполняет тест на самую длинную последовательность единиц в блоке.
+    :param sequence: бинарная последовательность
+    :param pi: список вероятностных констант
+    :return: P-значение теста
+    """
+    blocks = []
+    for i in range(0, len(sequence), 8):
+        block = sequence[i:i + 8]
+        blocks.append(block)
+
+    v = [0, 0, 0, 0]
+
+    for block in blocks:
+        max_len = 0
+        cur_len = 0
+
+        for bit in block:
+            if bit == '1':
+                cur_len += 1
+                max_len = max(max_len, cur_len)
+            else:
+                cur_len = 0
+
+        match max_len:
+            case x if x <= 1:
+                v[0] += 1
+            case 2:
+                v[1] += 1
+            case 3:
+                v[2] += 1
+            case _:
+                v[3] += 1
+
+        chi2 = sum(((v[i] - 16 * pi[i]) ** 2) / (16 * pi[i]) for i in range(4))
+        p_value = gammaincc(3 / 2, chi2/ 2)
+
+        return p_value
